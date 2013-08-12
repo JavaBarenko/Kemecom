664794e602722d10c3fff1ad1b2443addab33442
Rafael Caetano <barenko@gmail.com>
2013-08-12 18:06:39 -0300

    lib k rearanjada. corrigido problema no logout.

--- Kemecom-static/src/js/k.js
+++ Kemecom-static/src/js/k.js
@@ -71,34 +71,5 @@
         }).responseText).successful;
     };
 
-
-    //Precisa ser na window, pois findZipCode chama globalmente
-    window.correiocontrolcep = function(valor) {
-        if (valor.erro) {
-            alert("CEP não encontrado!");
-            return;
-        }
-
-        amplify.publish("findZipCode/" + valor.cep, valor);
-    };
-
-    K.findZipCode = function(zipCode, callback, context) {
-        var url = "http://cep.correiocontrol.com.br/" + zipCode + ".js";
-        var scr = $('<script>');
-        scr.attr('id', 'cep' + zipCode);
-        scr.attr('src', url);
-        scr.attr('charset', 'utf-8');
-        $('#tartaro').append(scr);
-
-        amplify.subscribe("findZipCode/" + zipCode, function(data) {
-            try {
-                callback.call(context, data);
-            } finally {
-                amplify.unsubscribe("findZipCode/" + zipCode, arguments.callee);
-                $('#cep' + zipCode).remove();
-            }
-        });
-    };
-
     $env.K = K;
 }(window, jQuery, amplify));
